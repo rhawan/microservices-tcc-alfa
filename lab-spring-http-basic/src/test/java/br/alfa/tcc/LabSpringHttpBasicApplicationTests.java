1a30087ce9fdd483cf476c22381511d7d711d369
Rhawan <rb.brenner10@gmail.com>
2017-04-04 13:14:35 -0300

    Criação inicial do projeto lab-spring-http-basic

---
+++ lab-spring-http-basic/src/test/java/br/alfa/tcc/LabSpringHttpBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package br.alfa.tcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabSpringHttpBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
