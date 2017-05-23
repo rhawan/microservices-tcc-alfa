9c381d4c713ceff40df5e8dd530c5d3728e5acee
Rhawan Brenner <rb.brenner10@gmail.com>
2017-05-22 21:43:32 -0300

    Implementando validação do formulário de cadastro de cliente

--- sales-microservices-authserver/src/main/java/br/alfa/sales/SalesMicroservicesAuthserverApplication.java
+++ sales-microservices-authserver/src/main/java/br/alfa/sales/SalesMicroservicesAuthserverApplication.java
@@ -2,10 +2,8 @@ package br.alfa.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SalesMicroservicesAuthserverApplication {
 	
 	public static void main(String[] args) {
